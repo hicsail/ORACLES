ed1d541720378ea85f6ac34466ece837c13a5f0c
Dany Fu <danyfu@bu.edu>
2021-03-28 16:09:32 -0400

    prove and verify pysnark generated constraints with zki/bulletproof and zki/bellman

--- aggregate_stats/pysnark/main.py
+++ aggregate_stats/pysnark/main.py
@@ -7,6 +7,13 @@
 
 from pysnark.runtime import snark, PrivVal
 from pysnark.fixedpoint import PrivValFxp
+import pysnark.zkinterface.backend
+
+# set modulus specific to Dalek Bulletproof
+#pysnark.zkinterface.backend.set_modulus(7237005577332262213973186563042994240857116359379907606001950938285454250989)
+
+# modulus specific to Bellman
+pysnark.zkinterface.backend.set_modulus(52435875175126190479447740508185965837690552500527637822603658699938581184513)
 
 @snark
 def average_gpa(gpas):
