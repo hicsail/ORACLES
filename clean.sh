1a14366655d34bb7bcd4de319b3ad104cf6896de
Glenn Xavier <gxavier@bu.edu>
2021-04-27 09:51:47 -0400

    Add cleanup script

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+find . -name "pysnark_*" -type f -delete
+find . -name "bellman-*" -type f -delete
+find . -name "computation.zkif" -type f -delete
+find . -name "bulletproofs-proof" -type f -delete
+find . -name "*.r1cs" -type f -delete
+find . -name "*.wtns" -type f -delete
