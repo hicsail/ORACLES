7ca0812d88caa7b0b16a15034e7d80e98f7c4eec
Glenn Xavier <gxavier@bu.edu>
2021-07-22 15:23:52 -0400

    Fix bug in degreesAwarded computation

--- src/degreesAwarded.py
+++ src/degreesAwarded.py
@@ -14,23 +14,21 @@ def compute(data, results, correct_hash):
     # Compute graduation rates by category
     output = {}
     for category in data:
-        arr2011 = [x == 1 for x in data[category]]
-        arr2013 = [x == 11 for x in data[category]]
+        graduated2011 = LinCombFxp(sum([x == 1 for x in data[category]]))
+        graduated2013 = LinCombFxp(sum([x == 11 for x in data[category]]))
         
-        graduated2011 = LinCombFxp(sum(arr2011))
-        graduated2013 = LinCombFxp(sum(arr2013))
-        length2011 = PrivVal(len(arr2011))
-        length2013 = PrivVal(len(arr2013))
+        length2011 = sum([x < 10 for x in data[category]])
+        length2013 = sum([x >= 10 for x in data[category]])
 
-        gr2011 = graduated2011 * 100 / (length2011 + (length2011 == 0))
-        gr2013 = graduated2013 * 100 / (length2013 + (length2013 == 0))
+        gradrate2011 = graduated2011 / (length2011 + (length2011 == 0))
+        gradrate2013 = graduated2013 / (length2013 + (length2013 == 0))
 
         output[category] = {
-            "Began in 2011": gr2011,
-            "Began in 2013": gr2013
+            "Began in 2011": gradrate2011,
+            "Began in 2013": gradrate2013
         }
 
-    # # Assert results are correct
+    # Assert results are correct
     for category in results:
         output[category]["Began in 2011"].assert_eq(results[category]["Began in 2011"])
         output[category]["Began in 2013"].assert_eq(results[category]["Began in 2013"])
@@ -42,8 +40,8 @@ if __name__ == '__main__':
     for category in data:
         students = []
         for student in data[category]:
-            num = student["Graduated"]
-            if student["Began"] == "2013":
+            num = int(student["Graduated"])
+            if student["Began"] == 2013:
                 num += 10
             students.append(PrivVal(num))
         data[category] = students
