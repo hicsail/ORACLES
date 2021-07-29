04dd5baa16968fe8a3bcd0c1076a680121e727b7
Glenn Xavier <gxavier@bu.edu>
2021-07-29 17:37:26 -0400

    Make input data distribution private

--- src/degreesAwarded.py
+++ src/degreesAwarded.py
@@ -11,40 +11,49 @@ def compute(data, results, correct_hash):
     hashed_data = poseidon_hash(flatten(data))
     [x.assert_eq(y) for (x,y) in zip(hashed_data, correct_hash)]
 
-    # Compute graduation rates by category
-    output = {}
-    for category in data:
-        graduated2011 = LinCombFxp(sum([x == 1 for x in data[category]]))
-        graduated2013 = LinCombFxp(sum([x == 11 for x in data[category]]))
-        
-        length2011 = sum([x < 10 for x in data[category]])
-        length2013 = sum([x >= 10 for x in data[category]])
+    # Constants
+    degree_lengths = [4, 6, 8]
+    start_years = [2011, 2013]
 
-        gradrate2011 = graduated2011 / (length2011 + (length2011 == 0))
-        gradrate2013 = graduated2013 / (length2013 + (length2013 == 0))
+    # Construct output dictionary in expected result format
+    output = {(str(y) + "-year"):{("Began in " + str(x)):0 for x in start_years} for y in degree_lengths}
 
-        output[category] = {
-            "Began in 2011": gradrate2011,
-            "Began in 2013": gradrate2013
-        }
+    # Populate output dictionary with graduation rates
+    for degree_length in degree_lengths:
+        for start_year in start_years:
+            # Construct integer representations for grads and non-grads
+            grad_repr = convert_to_int(True, start_year, degree_length)
+            non_grad_repr = convert_to_int(False, start_year, degree_length)
+
+            # Count number of grads and non-grads
+            grads = LinCombFxp(sum([x == grad_repr for x in data]))
+            non_grads = LinCombFxp(sum([x == non_grad_repr for x in data]))
+            total = grads + non_grads
+
+            grad_rate = grads / (total + (total == 0))
+
+            output[str(degree_length) + "-year"]["Began in " + str(start_year)] = grad_rate
 
     # Assert results are correct
     for category in results:
         output[category]["Began in 2011"].assert_eq(results[category]["Began in 2011"])
         output[category]["Began in 2013"].assert_eq(results[category]["Began in 2013"])
 
+def convert_to_int(graduated, start_year, degree_length):
+    student_repr = degree_length * 1000 
+    student_repr += (start_year % 100) * 10
+    student_repr += int(graduated)
+    return student_repr
+
 if __name__ == '__main__':
     runtime.bitlength = 17
 
     data = json.load(open('data/Degrees Awarded/data.json', 'r'))
-    for category in data:
-        students = []
-        for student in data[category]:
-            num = int(student["Graduated"])
-            if student["Began"] == 2013:
-                num += 10
-            students.append(PrivVal(num))
-        data[category] = students
+    # Create integer representation of data
+    data = [convert_to_int(x["Graduated"], x["Began"], int(x["Degree Type"][0])) for x in data]
+    # Construct private witness variables
+    data = [PrivVal(x) for x in data]
+
     results = json.load(open('data/Degrees Awarded/results.json', 'r'))
     correct_hash = json.load(open('data/Degrees Awarded/hash.json', 'r'))
 
--- src/netPriceByIncome.py
+++ src/netPriceByIncome.py
@@ -1,7 +1,11 @@
 import json
 from common import flatten, count_ops
+from pysnark import runtime
 from pysnark.runtime import snark, PrivVal
 from pysnark.poseidon_hash import poseidon_hash
+
+INCOME_BRACKETS = [(0, 30000), (30001, 48000), (48001, 75000), (75001, 110000), (110001,)]
+BRACKET_STRINGS = ["$0 to $30,000", "$30,001 to $48,000", "$48,001 to $75,000", "$75,001 to $110,000", "$110,001 and more"]
 
 @snark
 def compute(data, results, correct_hash):
@@ -9,15 +13,31 @@ def compute(data, results, correct_hash):
     hashed_data = poseidon_hash(flatten(data))
     [x.assert_eq(y) for (x,y) in zip(hashed_data, correct_hash)]
 
-    #  Begin with just 2016-2017
-    output = {}
+    output = {year:{} for year in data}
     for year in data:
         output[year] = {}
-        for income in data[year]:
-            year_data = data[year][income]
-            total = sum(year_data)
-            length = len(year_data)
-            output[year][income] = total / length
+
+        # Compute average net tuition for all income brackets except last
+        for i in range(len(INCOME_BRACKETS) - 1):
+            (bracket_min, bracket_max) = INCOME_BRACKETS[i]
+
+            # Select students in the current bracket
+            select_bit = [(bracket_min <= x) & (x <= bracket_max) for x in data[year]["Incomes"]]
+            # Get the total net tuition paid by current bracket
+            total = sum([x*y for (x,y) in zip(select_bit, data[year]["Tuitions"])])
+            # Count the number of students in the current bracket
+            num_students = sum(select_bit)
+
+            # Calculate average tuition paid by current bracket
+            average_net_tuition = total / (num_students + (num_students == 0))
+            output[year][BRACKET_STRINGS[i]] = average_net_tuition
+
+        # Compute average net tuition for $110,001+ bracket
+        select_bit = [x >= 110001 for x in data[year]["Incomes"]]
+        total = sum([x*y for (x,y) in zip(select_bit, data[year]["Tuitions"])])
+        num_students = sum(select_bit)
+        average_net_tuition = total / (num_students + (num_students == 0))
+        output[year][BRACKET_STRINGS[4]] = average_net_tuition
 
     # Check equality
     for year in results:
@@ -25,10 +45,19 @@ def compute(data, results, correct_hash):
             output[year][income].assert_eq(results[year][income])
 
 if __name__ == '__main__':
-    data = json.load(open('data/Net Price by Income/data.json', 'r'))
-    for year in data:
-        for income in data[year]:
-            data[year][income] = list(map(PrivVal, data[year][income]))
+    runtime.bitlength = 20
+
+    raw_data = json.load(open('data/Net Price by Income/data.json', 'r'))
+
+    data = {year:{} for year in raw_data}
+    for year in raw_data:
+        incomes = [PrivVal(x["Income"]) for x in raw_data[year]]
+        tuitions = [PrivVal(x["Net Tuition"]) for x in raw_data[year]]
+        data[year] = {
+            "Incomes": incomes,
+            "Tuitions": tuitions
+        }
+
     results = json.load(open ('data/Net Price by Income/results.json', 'r'))
     correct_hash = json.load(open('data/Net Price by Income/hash.json', 'r'))
 
